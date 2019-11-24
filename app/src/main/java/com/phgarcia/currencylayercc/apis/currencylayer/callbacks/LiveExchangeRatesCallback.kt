607c9ec1088c76d59f841bb5d0085e8d4ab88d9f
Pedro Garcia <garcia.ph@gmail.com>
2019-11-24 18:23:13 -0300

    Added conversion rates for conversions between non-USD currencies
    The in-app calculation is necessary because Currencylayer API free account only provides exchange rates from USD to other currencies.

--- app/src/main/java/com/phgarcia/currencylayercc/apis/currencylayer/callbacks/LiveExchangeRatesCallback.kt
+++ app/src/main/java/com/phgarcia/currencylayercc/apis/currencylayer/callbacks/LiveExchangeRatesCallback.kt
@@ -27,22 +27,51 @@ class LiveExchangeRatesCallback(
         response: Response<LiveExchangeRatesResponse>
     ) {
         response.body()?.let { it ->
+            val usdRates: MutableMap<String, Double> = mutableMapOf()
             it.quotes.forEach { exchangeRate ->
-                GlobalScope.launch { addExchangeRateToDB(exchangeRate) }
+                /*
+                 * Since the source of all exchange rates is USD (due to API limitations when
+                 * using a free account), we can add all rates to a map in order to calculate
+                 * the exchange rates between them.
+                 */
+                val source = exchangeRate.key.slice(0..2)
+                val target = exchangeRate.key.slice(3..5)
+                usdRates[target] = exchangeRate.value
+                GlobalScope.launch {
+                    addExchangeRateToDB(source, target, exchangeRate.value)
+                }
+            }
+
+            /*
+             * In order to calculate the exchange rate from a non-USD currency to another
+             * non-USD currency, we first calculate the value in USD from the source currency
+             * and then convert from USD to the target currency. E.g.:
+             *      EUR -> BRL becomes EUR -> USD -> BRL
+             */
+            val currencies = usdRates.keys
+            for (i in 0 until currencies.size) {
+                val source = currencies.elementAt(i)
+                GlobalScope.launch {
+                    for (j in i until currencies.size) {
+                        val target = currencies.elementAt(j)
+                        if (usdRates[source] != null && usdRates[target] != null) {
+                            val exchangeRate = usdRates[target]?.times(1 / (usdRates[source]?: -1.0))
+                            if (exchangeRate != null && exchangeRate > 0) {
+                                addExchangeRateToDB(source, target, exchangeRate)
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 
-    private suspend fun addExchangeRateToDB(exchangeRate: Map.Entry<String, Double>)
+    private suspend fun addExchangeRateToDB(source: String, target: String, rate: Double)
             = withContext(Dispatchers.IO) {
-        val source = exchangeRate.key.slice(0..2)
-        val target = exchangeRate.key.slice(3..5)
-        GlobalScope.run {
-            databaseInstance.exchangeRatesDAO
-                .insert(ExchangeRateEntity("$source$target", source, target, exchangeRate.value))
-            databaseInstance.exchangeRatesDAO
-                .insert(ExchangeRateEntity("$target$source", target, source, 1 / exchangeRate.value))
-        }
+        databaseInstance.exchangeRatesDAO
+            .insert(ExchangeRateEntity("$source$target", source, target, rate))
+        databaseInstance.exchangeRatesDAO
+            .insert(ExchangeRateEntity("$target$source", target, source, 1 / rate))
     }
 
 }
